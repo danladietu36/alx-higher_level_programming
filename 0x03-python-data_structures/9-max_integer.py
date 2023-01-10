74c703b81191ec8b00955431e3a3c952257ebd9e
Danladi Etu <danladietugithub@gmail.com>
2023-01-10 22:10:10 +0000

    9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,8 +3,8 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return "None"
     else:
-        max = my_list[0]
+        max_num = my_list[0]
         for i in range(len(my_list)):
             if my_list[i] > max:
-                max = my_list[i]
-     return max
+                max_num = my_list[i]
+     return max_num
