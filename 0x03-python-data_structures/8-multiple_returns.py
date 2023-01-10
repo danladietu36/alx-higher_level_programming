8ebb91dbcf13282e45625adf04b923c8b02cb604
Danladi Etu <danladietugithub@gmail.com>
2023-01-10 23:25:34 +0000

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     my_tuple = ()
-    for i in range(len(sentence))
-        if len(sentence) == 0:
-            my_tuple = 0, "None"
-        else:
-            my_tuple = len(sentence), sentence[0]
+    if len(sentence) == 0:
+        my_tuple = 0, "None"
+    else:
+        my_tuple = len(sentence), sentence[0]
     return my_tuple
