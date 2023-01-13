f73802c3d03e08cf541544f94e1be1bdcd7819fe
Danladi Etu <danladietugithub@gmail.com>
2023-01-13 13:11:10 +0000

    0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    while (matrix):
+        for row in matrix:
+            x = [x]
+            for col in row:
+                new_matrix = x**2
+        return new_matrix
