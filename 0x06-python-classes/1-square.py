091dc8b791094334e5fcf9a351b6e17dec89826f
Danladi Etu <danladietugithub@gmail.com>
2023-01-25 20:35:59 +0000

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
-class square:
+# 1-square.py by Danladi
+
+
+
+class Square:
+    """Represents a square"""
+
     def __init__(self, size):
+        """Initializing this square class
+        Args: size - represnets the size of the square defined
+        """
+
         self.__size = size
