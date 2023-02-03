4397f1f092ccbc2e88a2230bf0f838160a1946fa
Danladi Etu <danladietugithub@gmail.com>
2023-02-03 15:58:43 +0000

    5-text_indentation.py

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+
+This module contain a function that indents texts
+
+"""
+
+def text_indentation(text):
+    """This function prints a text with two new lines after each ",", "?", or ":"
+    Args:
+        text(str): The string to be printed
+
+    Raises:
+        TypeError("text must be a string")
+
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    count = 0
+    while count < len(text) and text[count] == " ":
+        count = count + 1
+
+    while count < len(text):
+        print(text[count], end="")
+        if text[count] == "\n" or text[count] in ".?:":
+            if text[count] in ".?:":
+                print("\n")
+            count = count + 1
+            while count < len(text) and text[count] == " ":
+                count = count + 1
+            continue
+            count = count + 1
