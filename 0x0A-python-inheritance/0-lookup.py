2ae5f41be608ac6da321c8e5c3b92cb5b57dc258
Danladi Etu <danladietugithub@gmail.com>
2023-02-08 13:15:44 +0000

    code_fix

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,5 +5,5 @@
 """
 
 def lookup(obj):
-    """This functions looks out for all attributes and methods of an object"""
+    """This functions looks out for all attributes and methods of an object."""
     return dir(obj)
