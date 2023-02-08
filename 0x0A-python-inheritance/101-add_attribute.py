77512718e2dbab7e4b55f5193fe0007da32d0d50
Danladi Etu <danladietugithub@gmail.com>
2023-02-08 13:58:32 +0000

    task13

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,7 +4,7 @@
 
 
 def add_attribute(obj, att, value):
-    """Add a new attribute to an object if possible
+    """Add a new attribute to an object if possible.
     """
     if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
