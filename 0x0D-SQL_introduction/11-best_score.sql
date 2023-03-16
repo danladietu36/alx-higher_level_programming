9f1a19dd741c5d3be9ccf32ab0848b8506987415
Danladi ETU <danladietugithub@gmail.com>
2023-03-16 11:12:54 -0700

    11-best_score.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- List all records with score greater or equal to 10
-SELECT * FROM second_table WHERE score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
