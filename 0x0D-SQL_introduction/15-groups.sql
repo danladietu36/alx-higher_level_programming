da4acd22a0a3740a14863c79fa28c6202cb13bb7
Danladi ETU <danladietugithub@gmail.com>
2023-03-16 14:30:36 -0700

    code_fix

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Counts the number of recorda that share the score value
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY score;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
