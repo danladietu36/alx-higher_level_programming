cc8073e2af9e04fec0d0d196d8fc8c3be1defc05
Danladi ETU <danladietugithub@gmail.com>
2023-03-16 10:49:16 -0700

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -12,7 +12,9 @@
 --id = 2, name = “Alex”, score = 3
 --id = 3, name = “Bob”, score = 14
 --id = 4, name = “George”, score = 8
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT,
+name VARCHAR(256),
+score INT);
 INSERT INTO second_table (id, name, score)
 VALUES (1, "John", 10),
 (2, "Alex", 3),
