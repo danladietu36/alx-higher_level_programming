ec6bc5d60c4556e9f731f8e938ba5c998b4c33aa
Danladi Etu <danladietu36@gmail.com>
2023-04-13 12:15:24 -0700

    code_fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,7 +3,7 @@ const argc = process.argv.length;
 
 
 if (argc > 2) {
-	console.log('Argument' + (argc > 3 ? 's' :' ') + 'found');
+	console.log('Argument' + (argc > 3 ? 's' : '') + 'found');
 } else {
 	console.log('No argument');
 }
