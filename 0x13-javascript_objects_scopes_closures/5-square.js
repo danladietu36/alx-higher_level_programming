c85f33d68c710a4e88b65380549b683064a01978
Danladi Etu <danladietu36@gmail.com>
2023-04-13 14:41:33 -0700

    code_fix

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 /**
- * square class that inherits from rectangle class
+ * Square class that inherits from rectangle class
  */
 const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
-	constructor (size {
-		super(size, size);
-	}
+  constructor (size) {
+    super(size, size);
+  }
 }
 
 module.exports = Square;
