2a138d6b8e7728ee716da77447de7a5253308883
Danladi Etu <danladietu36@gmail.com>
2023-04-13 13:13:17 -0700

    code_fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,9 +2,9 @@
 const x = process.argv[2];
 
 if (!parseInt(x)) {
-	console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-	for (let i = 0; i < x; i++) {
-		console.log('C is fun');
-	}
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
