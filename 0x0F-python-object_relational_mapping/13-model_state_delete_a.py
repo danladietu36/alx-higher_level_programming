a8f7962183d9324a998ee9821a648b74e29515dd
Danladi Etu <danladietu36@gmail.com>
2023-04-22 09:30:19 -0700

    added

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     """
 
     db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-            argv[1], argv[2], argv[3],)
+            argv[1], argv[2], argv[3])
     engine = create_engine(db_url)
     Session = sesssionmaker(bind=engine)
 
