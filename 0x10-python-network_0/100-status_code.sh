4e28e68db4a275dd2c6ed56291d58f9f08784173
Danladi Etu <danladietu36@gmail.com>
2023-04-30 02:20:49 -0700

    added

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that sends a request to a URL passed as an argument and display only status code
-curl -s -I "%{http_code}" "$1"
+curl -s -o /dev/null -w "%{http_code}" "$1"
