ad2b94a3fff1ec8edcf36f3957c92f1c3cff8563
Danladi Etu <danladietu36@gmail.com>
2023-04-30 00:56:09 -0700

    added

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # Bash script that takes in a URL, sends a request
 # to that URL, and displays the size of the body of the response
 name=URL
-curl -sI $name | grep -i Content-Length 
+curl -sI "$1" | wc -c 
