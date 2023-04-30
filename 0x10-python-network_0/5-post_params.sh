ecbad0354b1c26cd677f8577b1c9791e673e1863
Danladi Etu <danladietu36@gmail.com>
2023-04-30 02:11:30 -0700

    added

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL,variables and sends a POST request
-curl -sX -H POST "email: test@gmail.com, subject:I will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
