a0acc19b3cdd2339ce262a437951e3decbcb1a54
Danladi Etu <danladietugithub@gmal.com>
2023-05-02 08:59:26 -0700

    added

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,14 +5,23 @@ Please list 10 commits (from the most recent to oldest) of the repository
 “rails” by the user “rails”
 You must use the GitHub API, here is the documentation
 https://developer.github.com/v3/repos/commits/
-Print all commits by: `<sha>: <author name>` (one by line)
+Print all commits by: `<sha>: <author name>` (one by line).
 """
-import sys
 import requests
-from requests.auth import HTTPBasicAuth
+import sys
 
+repo_name = sys.argv[1]
+owner_name = sys.argv[2]
 
-if __name__ == "__main__":
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    response = requests.get("https://api.github.com/user", auth=auth)
-    print(response.json().get("id"))
+url = f'https://api.github.com/repos/{owner_name}/{repo_name}/commits'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    commits = response.json()[:10]
+    for commit in commits:
+        sha = commit['sha']
+        author = commit['commit']['author']['name']
+        print(f'{sha}: {author}')
+else:
+    print(f'Error: {response.status_code}')
