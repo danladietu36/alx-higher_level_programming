2224701882861007b3325ae69b7738f90a44856f
Danladi Etu <danladietugithub@gmal.com>
2023-05-02 06:26:58 -0700

    added

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     req = urllib.request.Request(url)
     try:
         with urllib.request.urlopen(req) as response:
-            response_body = response.read().decode('utf-8')
+            response_body = response.read().decode('ascii')
             print(response_body)
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
