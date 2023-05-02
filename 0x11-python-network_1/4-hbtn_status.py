26433bd057fb17f4bd00484df916a7e46beaca23
Danladi Etu <danladietugithub@gmal.com>
2023-05-02 07:54:43 -0700

    added

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,7 +8,6 @@ import requests
 if __name__ == '__main__':
     url = 'https://alx-intranet.hbtn.io/status'
     url_response = requests.get(url)
-    response_body = url_response.json()
 
     print('Body response:')
     print('\t- type: {}'.format(type(response_body)))
