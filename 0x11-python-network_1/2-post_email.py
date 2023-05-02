2eb8bf6f14f009318be5a74f58feccc6fb0e3c9b
Danladi Etu <danladietugithub@gmal.com>
2023-05-02 05:39:34 -0700

    added

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,10 +8,11 @@ import urllib.request
 import urllib.parse
 import sys
 
+if __name__ == "__main__"
 url = sys.argv[1]
 email = sys.argv[2]
 
-my_data = urllib.parse.urllencode({'email': email}).encode('utf-8')
+my_data = urllib.parse.urllencode({'email': email}).encode('ascii')
 req = urllib.request.Request(url, my_data)
 with urllib.request.urlopen(req) as response:
     response_body = response.read().decode('utf-8')
