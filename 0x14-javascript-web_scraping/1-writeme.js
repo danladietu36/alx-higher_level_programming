eca327f997550505693622e4feaeb5ce8f348a3a
Danladi Etu <danladietugithub@gmal.com>
2023-05-24 13:01:47 -0700

    added

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const file_name = process.argv[2];
+const fileName = process.argv[2];
 const data = process.argv[3];
 
-fs.writeFile(file_name, data, 'utf-8', (error) => {
+fs.writeFile(fileName, data, 'utf-8', (error) => {
   if (error) {
     console.log(error);
   }
