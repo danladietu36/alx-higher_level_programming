3fbf24f2c9bd76cbd3ad615190ecea8902be7ef7
Danladi Etu <danladietu36@gmail.com>
2023-05-24 06:02:04 -0700

    added

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,11 +4,10 @@ const first_arg = require("fs");
 const file_name = process.argv[2];
 
 
-first_arg.readFile(filename, "utf-8", (error, content) =>
+first_arg.readFile(filename, "utf-8", (error, content) => {
 	if (error) {
-		console.log(error)
-	}
-	else {
-		console.log(context);
+	  console.log(error)
+	} else {
+	  console.log(context);
 	}
 });
