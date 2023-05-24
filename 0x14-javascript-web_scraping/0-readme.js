ac1c053d4ec7f2111ef967d45d470093f6e60041
Danladi Etu <danladietugithub@gmal.com>
2023-05-24 12:14:07 -0700

    added

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,7 +6,7 @@ const file_name = process.argv[2];
 
 fs.readFile(file_name, "utf-8", (err, content) => {
 	if (err) {
-	  console.log(err)
+	  console.log(err);
 	} else {
 	  console.log(content);
 	}
